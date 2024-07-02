7637481a5278f55223801356524a626b0549aae9
adimerpaul <adimer101@gmail.com>
2024-07-02 05:48:01 -0400

    colcaodo el nombre correcto y loagin

--- front/src/pages/viajes/ViajesIndex.vue
+++ front/src/pages/viajes/ViajesIndex.vue
@@ -3,17 +3,17 @@
     <q-card>
       <div class="row">
         <div class="col-6 col-md-2 q-pa-xs">
-          <q-input v-model="fechaInicio" label="Fecha Inicio" type="date" outlined dense />
+          <q-input v-model="fechaInicio" label="Fecha Inicio" type="date" outlined dense :loading="loading" />
         </div>
         <div class="col-6 col-md-2 q-pa-xs">
-          <q-input v-model="fechaFin" label="Fecha Fin" type="date" outlined dense />
+          <q-input v-model="fechaFin" label="Fecha Fin" type="date" outlined dense :loading="loading" />
         </div>
         <div class="col-12 col-md-3 q-pa-xs">
-          <q-input v-model="filter" label="Buscar" outlined dense />
+          <q-input v-model="filter" label="Buscar" outlined dense :loading="loading" />
         </div>
         <div class="col-12 col-md-5 q-pa-xs text-right">
-          <q-btn color="indigo" label="Buscar" @click="viajeGet" no-caps icon="search" />
-          <q-btn color="green" label="Agregar" @click="viajeAdd" no-caps icon="add_circle_outline" />
+          <q-btn color="indigo" label="Buscar" @click="viajeGet" no-caps icon="search" :loading="loading"/>
+          <q-btn color="green" label="Agregar" @click="viajeAdd" no-caps icon="add_circle_outline" :loading="loading"/>
         </div>
       </div>
     </q-card>
