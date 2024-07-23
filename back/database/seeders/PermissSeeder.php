06c36e9deb15bb32a353734c2d2ec761d25360db
adimerpaul <adimer101@gmail.com>
2024-07-23 06:29:16 -0400

    colcoado permisos para el menu

--- back/database/seeders/PermissSeeder.php
+++ back/database/seeders/PermissSeeder.php
@@ -13,43 +13,33 @@ class PermissSeeder extends Seeder
      * Run the database seeds.
      */
     public function run(): void{
-        Permission::create(['name' => 'ver ventas', 'group' => 'ventas']);
-        Permission::create(['name' => 'crear ventas', 'group' => 'ventas']);
-        Permission::create(['name' => 'editar ventas', 'group' => 'ventas']);
-        Permission::create(['name' => 'anular ventas', 'group' => 'ventas']);
-
-        Permission::create(['name' => 'ver productos', 'group' => 'productos']);
-        Permission::create(['name' => 'crear productos', 'group' => 'productos']);
-        Permission::create(['name' => 'editar productos', 'group' => 'productos']);
-        Permission::create(['name' => 'eliminar productos', 'group' => 'productos']);
-
-        Permission::create(['name' => 'ver clientes', 'group' => 'clientes']);
-        Permission::create(['name' => 'crear clientes', 'group' => 'clientes']);
-        Permission::create(['name' => 'editar clientes', 'group' => 'clientes']);
-        Permission::create(['name' => 'eliminar clientes', 'group' => 'clientes']);
-
+        Permission::create(['name' => 'ver inicio', 'group' => 'ventas']);
         Permission::create(['name' => 'ver usuarios', 'group' => 'usuarios']);
-        Permission::create(['name' => 'crear usuarios', 'group' => 'usuarios']);
-        Permission::create(['name' => 'editar usuarios', 'group' => 'usuarios']);
-        Permission::create(['name' => 'eliminar usuarios', 'group' => 'usuarios']);
+        Permission::create(['name' => 'ver empresas', 'group' => 'empresas']);
+        Permission::create(['name' => 'ver botes', 'group' => 'botes']);
+        Permission::create(['name' => 'ver tripulantes', 'group' => 'tripulantes']);
+        Permission::create(['name' => 'ver clientes', 'group' => 'clientes']);
+        Permission::create(['name' => 'ver viajes', 'group' => 'viajes']);
+        Permission::create(['name' => 'ver ventas', 'group' => 'ventas']);
+        Permission::create(['name' => 'ver deudores', 'group' => 'deudores']);
 
-        $user = User::find(1);
-
-        $user->givePermissionTo('ver ventas');
-        $user->givePermissionTo('crear ventas');
-        $user->givePermissionTo('editar ventas');
-        $user->givePermissionTo('anular ventas');
-        $user->givePermissionTo('ver productos');
-        $user->givePermissionTo('crear productos');
-        $user->givePermissionTo('editar productos');
-        $user->givePermissionTo('eliminar productos');
-        $user->givePermissionTo('ver clientes');
-        $user->givePermissionTo('crear clientes');
-        $user->givePermissionTo('editar clientes');
-        $user->givePermissionTo('eliminar clientes');
-        $user->givePermissionTo('ver usuarios');
-        $user->givePermissionTo('crear usuarios');
-        $user->givePermissionTo('editar usuarios');
-        $user->givePermissionTo('eliminar usuarios');
+//        $user = User::find(1);
+//
+//        $user->givePermissionTo('ver ventas');
+//        $user->givePermissionTo('crear ventas');
+//        $user->givePermissionTo('editar ventas');
+//        $user->givePermissionTo('anular ventas');
+//        $user->givePermissionTo('ver productos');
+//        $user->givePermissionTo('crear productos');
+//        $user->givePermissionTo('editar productos');
+//        $user->givePermissionTo('eliminar productos');
+//        $user->givePermissionTo('ver clientes');
+//        $user->givePermissionTo('crear clientes');
+//        $user->givePermissionTo('editar clientes');
+//        $user->givePermissionTo('eliminar clientes');
+//        $user->givePermissionTo('ver usuarios');
+//        $user->givePermissionTo('crear usuarios');
+//        $user->givePermissionTo('editar usuarios');
+//        $user->givePermissionTo('eliminar usuarios');
     }
 }
