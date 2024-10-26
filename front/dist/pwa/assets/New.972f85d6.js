52ffd486a7d7b6a1515191848afdc0cea4c3f465
adimer <adimer101@gmail.com>
2024-10-26 04:11:17 -0400

    creado version

---
+++ front/dist/pwa/assets/New.972f85d6.js
@@ -0,0 +1,1 @@
+import{Q as e}from"./QPage.ef29aaa2.js";import{_ as t,Q as a,R as s,S as r,V as o}from"./index.d0cdc383.js";const c={name:"NewPage",data(){return{}},methods:{}},n=o("div",{class:"row justify-center"},null,-1);function _(d,f,i,l,p,u){return a(),s(e,null,{default:r(()=>[n]),_:1})}var w=t(c,[["render",_]]);export{w as default};
